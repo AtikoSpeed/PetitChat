1389cbdf07bd369a40ea4492074d8b1e9b17f26d
root <root@atakan.tech>
2024-10-16 16:47:02 -0400

    websocket working

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import { WebSocketServer } from "ws";
 import url from "url";
 import https from "https";
 import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
 
 const SSLcert = {
   key: fs.readFileSync("/etc/letsencrypt/live/atakan.tech/privkey.pem"),
@@ -12,9 +13,20 @@ const server = https.createServer(SSLcert, app);
 const port = 3000;
 const wsServer = new WebSocketServer({ server });
 
+const connections = {};
+const users = {};
+
 wsServer.on("connection", (con, req) => {
   const { username } = url.parse(req.url, true).query;
+  const uuid = uuidv4();
   console.log(username);
+  console.log(uuid);
+
+  connections[uuid] = con;
+
+  users[uuid] = {
+    username,
+  };
 });
 
 server.listen(port, () => {
