b30506882d5cfd3d76fa35d5fb07b261f83df1ba
root <root@atakan.tech>
2024-10-16 15:35:28 -0400

    added websocket, merged https.js and index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,22 @@
 import app from "./server.js";
-import httpsServer from "./https.js";
-const port = 3000;
+import { WebSocketServer } from "ws";
+import url from "url";
+import https from "https";
+import fs from "fs";
 
-httpsServer(app).listen(port, () => {
-  console.log(`Server is running on https://atakan.tech:${port}/`);
+const SSLcert = {
+  key: fs.readFileSync("/etc/letsencrypt/live/atakan.tech/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/atakan.tech/fullchain.pem"),
+};
+const server = https.createServer(SSLcert, app);
+const port = 3000;
+const wsServer = new WebSocketServer({ server });
+
+wsServer.on("connection", (con, req) => {
+  const { username } = url.parse(req.url, true).query;
+  console.log(username);
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on https://atakan.tech:${port}`);
 });
