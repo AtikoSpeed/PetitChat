ae4ddc678fca2a3b25bbc39de18c838ed95450c8
AtikoSpeed <atikotemizkan@gmail.com>
2024-10-17 15:01:28 -0400

    created basic mvp

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,7 +1,48 @@
+import useWebSocket from "react-use-websocket";
+import { useContext, useEffect, useState } from "react";
+import { Username } from "./App";
+
 export default function Chat() {
+  const [username] = useContext(Username);
+  const WS_URL = "wss://atakan.tech:3000";
+
+  const webSocket = useWebSocket(WS_URL, {
+    share: true,
+    queryParams: { username },
+  });
+
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    webSocket.lastJsonMessage
+      ? setMessages([...messages, webSocket.lastJsonMessage])
+      : "";
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [webSocket.lastJsonMessage]);
+
+  const mappedMsg = messages.map((msg) => {
+    return (
+      <>
+        <p key={msg}>
+          <b>{msg.username}: </b>
+          {msg.message}
+        </p>
+      </>
+    );
+  });
+
   return (
     <>
-      <h1>Chat</h1>
+      <h1>{username} Chatting</h1>
+      <div>{mappedMsg}</div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          webSocket.sendJsonMessage(e.target.submitMessage.value);
+        }}
+      >
+        <input type="text" id="submitMessage" name="submitMessage" />
+        <button type="submit">Send</button>
+      </form>
     </>
   );
 }
--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -7,7 +7,17 @@ export default function Login() {
   return (
     <>
       <div>
-        <h1>Bonjour</h1>
+        <h1>Enter your username</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setUsername(e.target.submittedUsername.value);
+            console.log(username);
+          }}
+        >
+          <input type="text" id="submittedUsername" name="submittedUsername" />
+          <button type="submit">Start chatting!</button>
+        </form>
       </div>
     </>
   );
